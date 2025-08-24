d65816a2a52c3fd7f75bd69d9060701efa27075d
sh4dow18 <ramses.solano.arias@gmail.com>
2025-08-23 23:03:25 -0600

    config: version to 0.10.0
    Change version from 0.9.0 to 0.10.0 with API connection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
             . Todos los Derechos Reservados.
           </p>
           {/* Last Update */}
-          <p className="text-xs sm:order-1 md:text-sm">05 de Agosto del 2025</p>
+          <p className="text-xs sm:order-1 md:text-sm">23 de Agosto del 2025</p>
         </footer>
       </body>
     </html>
